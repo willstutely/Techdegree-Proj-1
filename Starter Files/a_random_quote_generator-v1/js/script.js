813bf4c28b3a4d72def644e16fd850e117153c8b
willstutely <jonathanandrewdrake@gmail.com>
2021-01-21 13:48:21 -0600

    added quotes and adjusted array

--- Starter Files/a_random_quote_generator-v1/js/script.js
+++ Starter Files/a_random_quote_generator-v1/js/script.js
@@ -35,31 +35,50 @@ const quotes = [
   type: 'TV Show',
   year: 2014
   },
-  {name: 'Calvin',
-  quote: "I find my life is a lot easier the lower I keep everyone’s expectations.",
-  source: 'Calvin and Hobbes',
-  type: 'Comic Strip',
-  year: 1988
+  {name: 'Lucille Bluth',
+  quote: "I don’t understand the question and I won’t respond to it.",
+  source: 'Arrested Development',
+  type: 'TV Show',
+  year: 2004
+  },
+  {name: 'Reginald Rose',
+  quote: "Well, it's not easy to stand alone against the ridicule of others. He gambled for support and I gave it to him.",
+  source: '12 Angry Men',
+  type: 'Movie',
+  year: 1957
   },
   {name: 'Cavlin',
-  quote: "I’ve been accused of being a lot of things. Inarticulate ain’t one of ‘em.",
+  quote: "I find my life is a lot easier the lower I keep everyone’s expectations.",
   source: 'Justified',
   type: 'TV Show',
   year: 2014
-},
+}
 ]
 
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote(arr) {
+  const randomNumber = Math.floor(Math.random() * arr.length) + 1;
+  return arr[randomNumber].quote;
+  }
+
+
+
+
+
 
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
 
+
+
+}
 
 
 /***
