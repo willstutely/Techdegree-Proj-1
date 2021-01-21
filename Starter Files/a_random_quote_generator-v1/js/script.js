ca3222aa6e06b4f7d3c8020be4f3a248a2e15f7d
willstutely <jonathanandrewdrake@gmail.com>
2021-01-21 10:47:36 -0600

    Adding quotes to the array

--- Starter Files/a_random_quote_generator-v1/js/script.js
+++ Starter Files/a_random_quote_generator-v1/js/script.js
@@ -10,7 +10,44 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {name: 'Charley',
+  quote: "Well you may not know this, but there's things that gnaw at a man worse than dying.",
+  source: 'Open Range',
+  type: 'Movie',
+  year: 2003
+  },
+  {name: 'Boss Spearman',
+  quote: "A woman like that makes a man think about settin' down roots, eh?",
+  source: 'Open Range',
+  type: 'Movie',
+  year: 2003
+  },
+  {name: 'The Dude',
+  quote: "Yeah, well, that’s just, like, your opinion, man.",
+  source: 'The Big Lebowski',
+  type: 'Movie',
+  year: 1998
+  },
+  {name: 'Boyd Crowder',
+  quote: "I’ve been accused of being a lot of things. Inarticulate ain’t one of ‘em.",
+  source: 'Justified',
+  type: 'TV Show',
+  year: 2014
+  },
+  {name: 'Calvin',
+  quote: "I find my life is a lot easier the lower I keep everyone’s expectations.",
+  source: 'Calvin and Hobbes',
+  type: 'Comic Strip',
+  year: 1988
+  },
+  {name: 'Cavlin',
+  quote: "I’ve been accused of being a lot of things. Inarticulate ain’t one of ‘em.",
+  source: 'Justified',
+  type: 'TV Show',
+  year: 2014
+},
+]
 
 
 /***
