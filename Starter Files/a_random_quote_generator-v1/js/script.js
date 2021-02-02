86ae6165ddeb2da18fef2057953f1749914afe20
willstutely <jonathanandrewdrake@gmail.com>
2021-02-02 00:33:19 -0600

    updated printQuote function with conditionals

--- Starter Files/a_random_quote_generator-v1/js/script.js
+++ Starter Files/a_random_quote_generator-v1/js/script.js
@@ -11,79 +11,79 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 const quotes = [
-  {name: 'Charley',
+  {source: 'Charley',
   quote: "Well you may not know this, but there's things that gnaw at a man worse than dying.",
-  source: 'Open Range',
+  citation: 'Open Range',
   type: 'Movie',
   year: 2003,
   image: "images/charley.jpg"
   },
-  {name: 'Boss Spearman',
+  {source: 'Boss Spearman',
   quote: "A woman like that makes a man think about settin' down roots, eh?",
-  source: 'Open Range',
+  citation: 'Open Range',
   type: 'Movie',
   year: 2003,
   image: "images/bossspearman.jpg"
   },
-  {name: 'The Dude',
+  {source: 'The Dude',
   quote: "Yeah, well, that’s just, like, your opinion, man.",
-  source: 'The Big Lebowski',
+  citation: 'The Big Lebowski',
   type: 'Movie',
   year: 1998,
   image: "images/thedude.jpg"
   },
-  {name: 'Sam Spade',
+  {source: 'Sam Spade',
   quote: "That’s good, because if you actually were as innocent as you pretend to be, we’d never get anywhere.",
-  source: 'The Maltese Falcon',
+  citation: 'The Maltese Falcon',
   type: 'Movie',
   year: 1941,
   image: "images/samspade.jpg"
   },
-  {name: 'Juror #9',
+  {source: 'Juror #9',
   quote: "Well, it's not easy to stand alone against the ridicule of others. He gambled for support and I gave it to him.",
-  source: '12 Angry Men',
+  citation: '12 Angry Men',
   type: 'Movie',
   year: 1957,
   image: "images/juror9.jpg"
   },
-  {name: 'Boyd Crowder',
+  {source: 'Boyd Crowder',
   quote: "I’ve been accused of being a lot of things. Inarticulate ain’t one of ‘em.",
-  source: 'Justified',
+  citation: 'Justified',
   type: 'TV Show',
   year: 2014,
   image: "images/boydcrowder.jpg"
   },
-  {name: 'Raylan Givens',
+  {source: 'Raylan Givens',
   quote: "What's next is entirely based on the next choice you make.",
-  source: 'Justified',
+  citation: 'Justified',
   type: 'TV Show',
   year: 2015,
   image: "images/raylangivens.jpg"
   },
-  {name: 'Lucille Bluth',
+  {source: 'Lucille Bluth',
   quote: "I don’t understand the question and I won’t respond to it.",
-  source: 'Arrested Development',
+  citation: 'Arrested Development',
   type: 'TV Show',
   year: 2004,
   image: "images/lucillebluth.jpg"
   },
-  {name: 'Calvin',
+  {source: 'Calvin',
   quote: "You know, Hobbes, some days even my lucky rocket ship underpants don't help.",
-  source: 'Calvin and Hobbes',
+  citation: 'Calvin and Hobbes',
   type: 'Comic Strip',
   year: 1994,
   image: "images/calvin.gif"
   },
-  {name: 'Calvin',
+  {source: 'Calvin',
   quote: "I find my life is a lot easier the lower I keep everyone’s expectations.",
-  source: 'Calvin and Hobbes',
+  citation: 'Calvin and Hobbes',
   type: 'Comic Strip',
   year: 1988,
   image: "images/calvin.gif",
   },
-  {name: 'Me',
+  {source: 'Me',
   quote: "When you have zero coding experience learning JavaScript can make your brain feel like a bowl of bland oatmeal. But the satisfaction of struggling through various challenges and figuring them out is like adding brown sugar, apples and pecans on top.  Suddenly, every bite becomes delicious.",
-  source: 'My Own Brain',
+  citation: 'My Own Brain',
   year: 'Right Now',
   image: "images/me.jpg"
   }
@@ -104,6 +104,7 @@ function getRandomQuote(arr) {
     randomNumber = Math.ceil(Math.random() * arr.length) - 1;
   } else (!usedQuotes.includes(randomNumber))
     usedQuotes.push(randomNumber);
+  
   if (usedQuotes.length >= arr.length) {
     usedQuotes = [];
   }
@@ -118,11 +119,24 @@ function getRandomQuote(arr) {
 
 function printQuote() {
   const quote = getRandomQuote(quotes)
-  const message = `
-    <img src="file:///Users/JonathanDrake%201/Desktop/TreeHouse/Projects/Techdegree-Proj-1/Starter%20Files/${quote.image}" alt="${quote.name}">
-    <p class="quote">${quote.quote}</p>
-    <p class="source">${quote.name}<span class="citation">${quote.source}</span><span class="year">${quote.year}</span></p>
-  `;
+  let message = '';
+  if (quote.image) {
+    message += `<img src="file:///Users/JonathanDrake%201/Desktop/TreeHouse/Projects/Techdegree-Proj-1/Starter%20Files/${quote.image}" alt="${quote.name}">
+    `;
+  }
+  if (quote.quote) {
+    message += `<p class="quote">${quote.quote}</p>
+    `;
+  }
+  if (quote.source) {
+    message += `<p class="source">${quote.source}`;
+  }
+  if (quote.citation) {
+    message += `<span class="citation">${quote.citation}</span>`;
+  }
+  if (quote.year) {
+    message += `<span class="year">${quote.year}</span></p>`;
+  }
   return document.querySelector('div').innerHTML = message; 
 }
 
